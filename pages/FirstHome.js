3d351878b23d5dd67b645d9c2542c55889f463db
andreyscott <andreyscott301@gmail.com>
2022-11-16 15:21:15 +0100

    fixed the build error

--- pages/FirstHome.js
+++ pages/FirstHome.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import FirstMain from "../components/Home/FirstMain/FirstMain";
-import MainSwiper from "../components/Home/MainSwiper/mainSwiper";
+import MainSwiper from "../components/Home/MainSwiper/MainSwiper";
 import SecMain from "../components/Home/SecMain/SecMain";
 import SecSwiper from "../components/Home/SecSwiper/SecSwiper";
 import TriMain from "../components/Home/TriMain/TriMain";
@@ -20,14 +20,14 @@ const FirstHome = ({ productList }) => {
 export default FirstHome;
 
 export const getServerSideProps = async () => {
-  // const res = await axios.get("https://course-api.com/react-store-products");
+  const res = await axios.get("http://localhost:3000/api/products");
+  // ("https://course-api.com/react-store-products");
   // console.log(res.data);
 
-  const response = await fetch("https://course-api.com/react-store-products");
-  if (!response.ok) throw new Error("Problem Getting Data");
-  const data = await response.json();
+  // const response = await fetch("https://course-api.com/react-store-products");
+  // if (!response.ok) throw new Error("Problem Getting Data");
+  // const data = await response.json();
   
-  // ("http://localhost:3000/api/products");
   return {
     props: {
       productList: response.data,
